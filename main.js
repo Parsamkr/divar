286de699dc7378e02dce3f4a7e769128195e89b0
Mohammad Parsa Mokhtarpour <parsa.mokhtarpour@yahoo.com>
2024-03-10 18:18:36 +0330

    making guard folder for authorization midleware , making the user module , and defining the access-key in cookie

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const SwaggerConfig = require("./src/config/swagger.config");
 const mainRouter = require("./src/app.route");
 const notFoundHandler = require("./src/common/exception/not-found.handler");
 const allExceptionHandler = require("./src/common/exception/all-exception.handler");
+const cookieParser = require("cookie-parser");
 dotenv.config();
 
 async function main() {
@@ -12,6 +13,7 @@ async function main() {
   require("./src/config/mongoose.config");
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
   app.use(mainRouter);
   SwaggerConfig(app);
   notFoundHandler(app);
--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,7 @@
 const autoBind = require("auto-bind");
 const authService = require("./auth.service");
 const { authMessage } = require("./auth.messages");
+const NodeEnv = require("../../common/constant/env.enum");
 
 class authController {
   #service;
@@ -10,10 +11,6 @@ class authController {
   }
   async sendOTP(req, res, next) {
     try {
-      require("crypto").randomBytes(48, function (err, buffer) {
-        var token = buffer.toString("hex");
-        console.log(token);
-      });
       const { mobile } = req.body;
       await this.#service.sendOTP(mobile);
       return res.json({ message: authMessage.sendOtpSuccessfully });
@@ -26,7 +23,13 @@ class authController {
     try {
       const { mobile, code } = req.body;
       const token = await this.#service.checkOTP(mobile, code);
-      return res.json({ message: authMessage.loginSuccessfully, token });
+      return res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === NodeEnv.PRODUCTION,
+        })
+        .status(200)
+        .json({ message: authMessage.loginSuccessfully, token });
     } catch (error) {
       next(error);
     }
